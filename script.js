0baf212c483ab84cc46cdbd903881cc328dbeb95
SailorTV <marin.lfv@iloud.com>
2025-01-22 14:51:59 +0100

    onenelelesfd

--- script.js
+++ script.js
@@ -153,10 +153,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Téléchargement du pack de texturess
     downloadButton.addEventListener('click', function () {
-        if (Object.keys(selectedTextures).length !== textureSequence.length) {
-            alert('Veuillez sélectionner toutes les textures.');
-            return;
-        }
+
 
         // Afficher le modal pour entrer le nom du pack
         packNameModal.style.display = 'block';
