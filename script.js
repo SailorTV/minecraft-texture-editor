3facffb3950c3d777b0014a2ec780eb0729756ae
SailorTV <marin.lfv@iloud.com>
2025-01-23 09:58:32 +0100

    test3

--- script.js
+++ script.js
@@ -206,7 +206,7 @@ document.addEventListener('DOMContentLoaded', function () {
                         }
                     }
                 } else if (textureType === 'armure_paladium') {
-                    const folderPath = `textures/${resolution}/armure_paladium/armurepaladium${i}/`;
+                    const folderPath = `textures/${resolution}/armure_paladium/armure_paladium${i}/`;
                     const files = [
                         'paladium_boots.png', 'paladium_leggings.png', 'paladium_chestplate.png', 'paladium_helmet.png',
                         'paladium_armor_1.png', 'paladium_armor_2.png',
